b6408d47f9ee3d8fba256f09edf70495c82816e2
Roman Romanovski <roman@lethops.com>
2019-11-25 22:19:44 +0300

    Updated storybook to parse .mdx docs.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,6 @@
 import { configure } from '@storybook/react'
 // automatically import all files ending in *.stories.js
-configure(require.context('../packages', true, /\.stories\.tsx?$/), module)
+configure(
+	[require.context('../packages', true, /\.stories\.tsx?$/), require.context('../docs', true, /\.stories\.mdx?$/)],
+	module
+)
