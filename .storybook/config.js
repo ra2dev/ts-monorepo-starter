9d0fabe77df06792f57b66b86a811af28e3b7e95
Roman Romanovski <roman@lethops.com>
2021-12-26 02:04:04 +0300

    Updated storybook, fixed audit issues.

--- .storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure } from '@storybook/react'
-// automatically import all files ending in *.stories.js
-configure(
-	[require.context('../packages', true, /\.stories\.tsx?$/), require.context('../docs', true, /\.stories\.mdx?$/)],
-	module
-)
---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const { lstatSync, readdirSync } = require('fs')
+
+const basePath = path.resolve(__dirname, '../', 'packages')
+const packages = readdirSync(basePath).filter((name) => lstatSync(path.join(basePath, name)).isDirectory())
+
+//import { configure } from '@storybook/react'
+//// automatically import all files ending in *.stories.js
+//configure(
+//	[require.context('../packages', true, /\.stories\.tsx?$/), require.context('../docs', true, /\.stories\.mdx?$/)],
+//	module
+//)
+
+module.exports = {
+	addons: [],
+	webpackFinal: async config => {
+		Object.assign(config.resolve.alias, {
+			...packages.reduce(
+				(acc, name) => ({
+					...acc,
+					[`@monoprefix/${name}`]: path.join(basePath, name, 'src'),
+				}),
+				{}
+			),
+		})
+		return config
+	}
+}
