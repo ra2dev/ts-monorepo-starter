db0f40229ddfa48a0053229896b0d05af2544d97
Roman Romanovski <roman@lethops.com>
2022-01-01 12:56:58 +0100

    Small update.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,6 @@
 const path = require('path');
-
-const {
-  lstatSync,
-  readdirSync
-} = require('fs');
-
 const basePath = path.resolve(__dirname, '../', 'packages');
-const packages = readdirSync(basePath).filter(name => lstatSync(path.join(basePath, name)).isDirectory()); //import { configure } from '@storybook/react'
-//// automatically import all files ending in *.stories.js
-//configure(
-//	[require.context('../packages', true, /\.stories\.tsx?$/), require.context('../docs', true, /\.stories\.mdx?$/)],
-//	module
-//)
-///Users/roman/opensource/ts-monorepo-starter/packages/ui-components/src/components/Button/Button.stories.tsx
+
 module.exports = {
   addons: [],
 	stories: ['../packages/ui-components/src/components/Button/Button.stories.tsx'],
